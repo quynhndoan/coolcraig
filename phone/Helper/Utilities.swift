ab744358f2d161c4793f0f79f6ea5511e9445432
quynhndoan <36650690+quynhndoan@users.noreply.github.com>
2020-08-15 00:07:18 -0400

    Create Utilities.swift

---
+++ phone/Helper/Utilities.swift
@@ -0,0 +1,75 @@
+//
+//  Utilities.swift
+//  customauth
+//
+//  Created by Christopher Ching on 2019-05-09.
+//  Copyright © 2019 Christopher Ching. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utilities {
+    
+    static func styleTextField(_ textfield:UITextField) {
+        
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
+        
+        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
+        
+        // Remove border on text field
+        textfield.borderStyle = .none
+        
+        // Add the line to the text field
+        textfield.layer.addSublayer(bottomLine)
+        
+    }
+    
+    static func styleFilledButton(_ button:UIButton) {
+        
+        // Filled rounded corner style
+        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.white
+    }
+    
+    static func styleHollowButton(_ button:UIButton) {
+        
+        // Hollow rounded corner style
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.black
+    }
+    
+    static func isPasswordValid(_ password : String) -> Bool{
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    static func storeKey(key: String, value: Any) {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(value, forKey: key)
+        userDefaults.synchronize()
+    }
+    
+    static func getKey(key: String, defaultValue:Any = "") -> Any {
+        if let value: AnyObject = UserDefaults.standard.object(forKey: key) as AnyObject? {
+            return value
+        }
+        return defaultValue
+    }
+    
+    static func deleteKey(key: String) -> Any {
+        UserDefaults.standard.removeObject(forKey: key)
+    }
+    
+    static func getDictValue(dict: [String: Any], forPath: String) -> [String: Any] {
+        return (dict as NSDictionary).value(forKeyPath: forPath) as! [String: Any]
+    }
+    
+    
+}
